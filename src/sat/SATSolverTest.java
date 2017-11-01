d5098a4c84963c513a15ffaaad6abcbd222581df
candace <candace_sng@mymail.sutd.edu.sg>
2017-11-01 17:05:47 +0800

    satsolvertest parsing and implemented print satisfiability and txt file

--- src/sat/SATSolverTest.java
+++ src/sat/SATSolverTest.java
@@ -1,22 +1,15 @@
 package sat;
 
-/*import static org.junit.Assert.*;
+/*
+ import static org.junit.Assert.*;
+ 
+ import org.junit.Test;
+ */
 
-import org.junit.Test;
-*/
+import sat.env.*;
+import sat.formula.*;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-import sat.env.Bool;
-import sat.env.Environment;
-import sat.formula.Clause;
-import sat.formula.Formula;
-import sat.formula.Literal;
-import sat.formula.NegLiteral;
-import sat.formula.PosLiteral;
+import java.io.*;
 
 
 public class SATSolverTest {
@@ -27,33 +20,28 @@ public class SATSolverTest {
     Literal nb = b.getNegation();
     Literal nc = c.getNegation();
 
+    // TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
+
+    public void testSATSolver1() {
+        // (a v b)
+        Environment e = SATSolver.solve(makeFm(makeCl(a, b)));
+        /*
+         assertTrue( "one of the literals should be set to true",
+         Bool.TRUE == e.get(a.getVariable())
+         || Bool.TRUE == e.get(b.getVariable())    );
+         
+         */
+    }
 
 
-	
-	// TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
-    
-	
-    public void testSATSolver1(){
-    	// (a v b)
-    	Environment e = SATSolver.solve(makeFm(makeCl(a,b))	);
-    	System.out.println(e);
-    	/*
-    	assertTrue( "one of the literals should be set to true",
-    			Bool.TRUE == e.get(a.getVariable())  
-    			|| Bool.TRUE == e.get(b.getVariable())	);
-    	 */	
+    public void testSATSolver2() {
+        // (~a)
+        Environment e = SATSolver.solve(makeFm(makeCl(na)));
+        /*
+         assertEquals( Bool.FALSE, e.get(na.getVariable()));
+         */
     }
-    
-    
-    public void testSATSolver2(){
-    	// (~a)
-    	Environment e = SATSolver.solve(makeFm(makeCl(na)));
-    	System.out.println(e);
-    	/*
-    	assertEquals( Bool.FALSE, e.get(na.getVariable()));
-    	*/
-    }
-    
+
     private static Formula makeFm(Clause... e) {
         Formula f = new Formula();
         for (Clause c : e) {
@@ -61,7 +49,7 @@ public class SATSolverTest {
         }
         return f;
     }
-    
+
     private static Clause makeCl(Literal... e) {
         Clause c = new Clause();
         for (Literal l : e) {
@@ -69,8 +57,91 @@ public class SATSolverTest {
         }
         return c;
     }
-    
-    
-    
-    
+
+    public static Formula parse(String filename) {
+        try {
+            BufferedReader br1 = new BufferedReader(new FileReader(filename));
+            String line1;
+            Clause clause = new Clause();
+            Formula formula = new Formula();
+
+            while ((line1 = br1.readLine()) != null) {
+                if (line1.equals("") || line1.charAt(0) == 'c' || line1.charAt(0) == 'p') {
+                    continue;
+                }
+                String[] literalStrings = line1.split("\\s+");
+                for (String literalString : literalStrings) {
+                    if (!literalString.equals("0")) {
+                        Literal literal;
+                        if (literalString.charAt(0) == '-') {
+                            literal = NegLiteral.make(literalString.substring(1));
+                        } else {
+                            literal = PosLiteral.make(literalString);
+                        }
+                        clause = clause.add(literal);
+                    } else {
+                        formula = formula.addClause(clause);
+                        clause = new Clause();
+                    }
+                }
+            }
+            return formula;
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found.");
+            return null;
+        } catch (java.io.IOException ex) {
+            System.out.println("Error reading file.");
+            return null;
+        }
+    }
+
+    public static void printOutput(String file) {
+        System.out.println("File: " + file);
+        Formula f= parse(file);
+
+        System.out.println("SAT solve starts!!!");
+        long started = System.nanoTime();
+        Environment e = SATSolver.solve(f);
+        long time = System.nanoTime();
+        long timeTaken = time - started;
+        System.out.println("Time: " + timeTaken/1000000.0 + "ms");
+        if (e != null) {
+            System.out.println("satisfiable");
+            File txtFile = new File("BoolAssignment" + ".txt");
+            int counter = 1;
+            while (txtFile.exists()){
+                counter++;
+                txtFile = new File("BoolAssignment" + (counter - 1) + ".txt");
+            }
+            if (!txtFile.exists()){
+                try (Writer writeFile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(txtFile), "utf-8"))){
+
+                    String s = e.toString().replace("Environment:[", "");
+                    s = s.replace("]", "");
+                    String[] lines = s.split(", ");
+                    for (String line: lines) {
+                        writeFile.write(line + "\r\n");
+                    }
+                    writeFile.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+        }
+        else {
+            System.out.println("unsatisfiable");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String largeSat = "./src/Project-2D/project-2d-starting/sampleCNF/largeSat.cnf";
+        String largeUnsat = "./src/Project-2D/project-2d-starting/sampleCNF/largeUnsat.cnf";
+        String s8Sat = "./src/Project-2D/project-2d-starting/sampleCNF/s8Sat.cnf";
+
+        printOutput(largeSat);
+        printOutput(largeUnsat);
+        printOutput(s8Sat);
+    }
 }
